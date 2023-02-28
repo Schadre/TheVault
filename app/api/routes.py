d625ddd61bd1d91a710c99c1a7e41de6a1a118ba
Schadre <103374729+Schadre@users.noreply.github.com>
2023-02-28 17:04:44 -0600

    Delete app/api

--- app/api/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request, jsonify, render_template
-from helpers import token_required
-from models import db, User, Book, book_schema, books_schema
-import requests 
-
-api = Blueprint('api',__name__, url_prefix='/api')
-
